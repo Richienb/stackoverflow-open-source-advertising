359f95de7591e7983e173f96567862d712c3ecef
Glitch (hello-express) <none>
2019-02-17 07:23:32 +0000

    🍫🐒 Checkpoint ./views/index.html:1729229/156 ./public/client.js:1729229/1 ./public/style.css:1729229/1 ./server.js:1729229/8967 ./package.json:1729229/2600 ./index.html:1729229/205

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 // init project
 const express = require('express');
 const app = express();
+const request = require("request");
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -13,10 +14,19 @@ app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+request({
+            url: "https://stackoverflow.com/ossads/300x250",
+            method: 'GET',
+            headers: {
+                'User-Agent': 'rosav (nodejs)'
+            }
+        },
+    function(error, response, body) {
+        response.send(body);
+    });
 });
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+    console.log('Your app is listening on port ' + listener.address().port);
 });
