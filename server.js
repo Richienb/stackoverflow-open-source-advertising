35aa0bb25c3995d5e0e79e90ef2229d573525aa4
Glitch (stackoverflow-open-source-advertising) <none>
2019-03-02 02:23:36 +0000

    🌧🚴 Checkpoint ./package.json:1729229/1024 ./server.js:1729229/5211

--- server.js
+++ server.js
@@ -2,45 +2,48 @@
 // where your node app starts
 
 // init project
-const express = require('express');
-const app = express();
-const https = require('https')
+const express = require("express")
+const app = express()
+const https = require("https")
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"))
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  
-  let width = request.query.width ? request.query.width : '300'
-  let height = request.query.height ? request.query.height : '250'
-  let extracss = ""
-  extracss += request.query.margin === 'true' ? "" : "<style>body, .ad-container { margin: 0 }</style>"
-  extracss += request.query.footer === 'false' ? "<style>div.footer { display: none }</style>" : ""
-  
-  https.get(`https://stackoverflow.com/ossads/${width}x${height}`, (resp) => {
-  let data = '';
+app.get("/", function(request, response) {
 
-  // A chunk of data has been recieved.
-  resp.on('data', (chunk) => {
-    data += chunk;
-  });
+    let width = request.query.width ? request.query.width : '300'
+    let height = request.query.height ? request.query.height : '250'
+    let extracss = ""
+    extracss += request.query.margin === 'true' ? "" : "<style>body, .ad-container { margin: 0 }</style>"
+    extracss += request.query.footer === 'false' ? "<style>div.footer { display: none }</style>" : ""
 
-  // The whole response has been received. Print out the result.
-  resp.on('end', () => {
-    response.send(data + extracss);
-  });
+    https.get({
+  hostname: `https://stackoverflow.com/ossads/${width}x${height}`,
+  headers: { "User-Agent": 'Mozilla/5.0' }
+}, (resp) => {
+        let data = ""
 
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
-});
-  
-});
+        // A chunk of data has been recieved.
+        resp.on("data", (chunk) => {
+            data += chunk
+        })
+
+        // The whole response has been received. Print out the result.
+        resp.on("end", () => {
+            response.send(data + extracss)
+        })
+
+    }).on("error", (err) => {
+        console.log("Error: " + err.message)
+    })
+
+})
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
-    // console.log('Your app is listening on port ' + listener.address().port);
-});
+    // console.log('Your app is listening on port ' + listener.address().port)
+})
